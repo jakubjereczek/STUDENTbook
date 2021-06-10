4acc511380062d5ea7388cd3683e847b953758eb
Jakub Jereczek <99jakubjereczek@gmail.com>
2021-06-10 21:55:01 +0200

    Infinity scroll

--- Client/src/pages/Main/components/PostsList.js
+++ Client/src/pages/Main/components/PostsList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useMemo } from 'react'
+import React, { useState, useEffect, useRef, useCallback } from 'react'
 import { Link } from 'react-router-dom';
 import { ContainerInside, UserIcon, Author, ButtonIcon, Text } from '../../../components/SharedStyles.css'
 import { AboutUser, DateString, PostContent, PostButtons } from './PostsList.css'
@@ -17,7 +17,7 @@ import EditPostPopup from './EditPostPopup';
 
 function PostsList({ currentUser }) {
     const [posts, setPosts] = useState([]);
-
+    const [visiblePosts, setVisiblePosts] = useState([]);
 
     const [hoverActive, setHoverActive] = useState(false);
     const [popupActive, setPopupActive] = useState(false);
@@ -31,17 +31,25 @@ function PostsList({ currentUser }) {
 
     const [loading, setLoading] = useState(true);
 
+    // pagination
+    // TODO: Pagination in Server side. 
+    // I should to get posts (example ten for page) with every scroll, not everythings.
+    const [hasMore, setHasMore] = useState(false);
+    const [currentSite, setCurrentSite] = useState(1);
+    const renderCount = 10;
+
     useEffect(async () => {
         if (!popupActive) {  // Startowo jest false - czyli wykona się na starcie + przy wyjściu z edycji, edytowaniu.
             const posts = await getAllPosts();
             setPosts(posts.data);
+            const visiblePosts = posts.data.slice(0, currentSite * renderCount);
+            setVisiblePosts(visiblePosts);
             setLoading(false);
         }
     }, [popupActive]) // W przypadku gdy zmienimy wartość popupu. 
 
 
     const onMouseEnter = (event, user) => {
-        console.log(event)
         setHoverCords({
             top: event.clientY + 10,
             left: event.clientX
@@ -55,7 +63,7 @@ function PostsList({ currentUser }) {
     }
 
     const deletePostAction = (post) => {
-        const postsUpdated = posts.filter(p => p != post);
+        const postsUpdated = posts.filter(p => p !== post);
         setPosts(postsUpdated)
         deletePost(post.postId)
             .then(() => {
@@ -70,13 +78,34 @@ function PostsList({ currentUser }) {
         setActivePost(post);
     }
 
-    const postsList = posts.map((post) => {
+    const observer = useRef();
+    const lastPostElementRef = useCallback(node => {
+        console.log(!loading && node);
+        if (loading)
+            return false;
+        if (observer.current) observer.current.disconnect();
+        observer.current = new IntersectionObserver(entries => {
+            if (entries[0].isIntersecting) {
+                console.log('visible');
+                setHasMore(visiblePosts < posts);
+                if (hasMore) {
+                    setCurrentSite(currentSite + 1);
+                    const visiblePosts = posts.slice(0, currentSite * renderCount);
+                    setVisiblePosts(visiblePosts);
+                }
+            }
+        });
+        if (node) observer.current.observe(node);
+
+    })
+
+    const postsList = visiblePosts.map((post, index) => {
         const user = post.Users;
         const university = post.Users.University;
 
         return (
             <React.Fragment>
-                <ContainerInside key={post.postId}>
+                <ContainerInside key={post.postId} ref={visiblePosts.length === index + 1 ? lastPostElementRef : null}>
                     <AboutUser>
                         <UserIcon />
                         <p>
@@ -91,11 +120,9 @@ function PostsList({ currentUser }) {
                             {post.userId === currentUser.userId && (
                                 <React.Fragment>
                                     <ButtonIcon onClick={() => deletePostAction(post)}>
-                                        {/* Edit */}
                                         <FaRegTimesCircle />
                                     </ButtonIcon>
                                     <ButtonIcon onClick={() => editPostAction(post)}>
-                                        {/* Delete */}
                                         <FaRegEdit />
                                     </ButtonIcon>
                                 </React.Fragment>)
@@ -104,7 +131,6 @@ function PostsList({ currentUser }) {
                     </PostContent>
                 </ContainerInside >
             </React.Fragment>
-
         )
     });
 
@@ -112,7 +138,6 @@ function PostsList({ currentUser }) {
         <React.Fragment>
             <AddPost setLoading={setLoading} setPosts={setPosts} posts={posts} />
             <EditPostPopup active={popupActive} setActive={setPopupActive} post={activePost} />
-
             {postsList}
             {hoverActive && <Hover active={hoverActive} user={hoverUser} style={hoverCords} />}
         </React.Fragment>
